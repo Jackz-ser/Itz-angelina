f7dd5c12ef38cf040d4e881cf1dbf5120b58697e
Jackz-ser <96305513+Jackz-ser@users.noreply.github.com>
2022-01-23 13:16:53 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,24 +11,24 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     var respoimage = await axios.get(config.LIZA, { responseType: 'arraybuffer' })
 
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('20210811_082543.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈ANGELINA≈≈≈≈≈≈≈≈*
  
-*owner number wa.me/918137829228*
+*owner number wa.me/918075641889?text=Hai%20Jackz%20Ser*
    
-*owner number wa.me/918075379950*
+*owner number wa.me/917994765065?text=Hai%20Broken%20Ser*
 
 
-*whatsapp group : https://chat.whatsapp.com/FVOdpPLaMvP24rIfTzCGof*
+*whatsapp group : https://chat.whatsapp.com/DqtTdFz3yTvDNi1QihM0EP*
 
 
-*githublink       _https://tinyurl.com/yjdej5a2_*
+*githublink   :  _github.com/Jackz-ser/Itz-angelina_*
 
 
-*audio commads    _https://tinyurl.com/ydvlpbol_*
+*audio commands  : _Audios comming soon_*
 
-*Bot Make Video ytube -https://youtu.be/s3ugOuxbQ_Q_*
+*Bot Make Video ytube - Video coming soon*
 
-*sticker commads  _https://tinyurl.com/yfpd23yn_*     
+*sticker commands : _Sticker coming soon_*     
 `}) 
 
 })); 
